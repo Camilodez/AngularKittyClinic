c29d7007df93bf071867c1728c2b480c74223f58
juanzulu <jumizusu@gmail.com>
2024-04-03 20:17:40 -0500

    routing

--- Project/src/app/app-routing.module.ts
+++ Project/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { CrearGatoComponent } from './crear-gato/crear-gato.component';
 import { ModificarGatoComponent } from './modificar-gato/modificar-gato.component';
 import { GatoComponent } from './gato/gato.component';
 import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: LandingComponent },
   { path: 'mascotas', component: GatoComponent },
   { path: 'crear-mascota', component: CrearGatoComponent },
   { path: 'modificar-mascota', component: ModificarGatoComponent },
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 
 ];
--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { LandingComponent } from './landing/landing.component';
     HeaderComponent,
     FooterComponent,
     LandingComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
