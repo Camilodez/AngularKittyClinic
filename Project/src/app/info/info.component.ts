c3c4e354d7f75d2da6eec0417902d474dd557288
juanzulu <jumizusu@gmail.com>
2024-04-04 16:30:07 -0500

    imagen gato

--- Project/src/app/info/info.component.ts
+++ Project/src/app/info/info.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { GatoService } from '../service/gato.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Gato } from '../models/gato.model';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-info',
@@ -7,4 +11,51 @@ import { Component } from '@angular/core';
 })
 export class InfoComponent {
 
+  constructor(private gatoService: GatoService, private route: ActivatedRoute, 
+    public formBuilder: FormBuilder, public router: Router) {
+
+    this.route.params.subscribe(params => {
+    this.displayinfo(parseInt(params['id']));
+      console.log(this.gato)
+
+    })
+
+    this.gatoForm = this.formBuilder.group({
+      nombre: [''],
+      raza: [''],
+      edad: [''],
+      foto: ['']
+    })
+
+  }
+
+  ngOnInit() {
+    this.gatoForm.patchValue(this.gato);
+    console.log(this.gato)
+  }
+
+  gatoForm!: FormGroup;
+
+  gato: Gato = {
+    id: 0,
+    nombre: '',
+    raza: '',
+    edad: 0,
+    foto: '',
+    enfermedad: '',
+    estado: true,
+  };
+
+  displayinfo(id: number) {
+    this.gato = this.gatoService.findById(id)!;
+  }
+
+  onSubmit() {
+    this.gato = this.gatoForm.value;
+    console.log(this.gato)
+    this.gatoService.actualizarGato(this.gato);
+    this.router.navigate(['/mascotas']);
+  }
+
 }
+
