ba00e686835cf1985cdf245ab2a9ae29a35704bd
juanzulu <jumizusu@gmail.com>
2024-04-03 22:58:14 -0500

    pagina lista avanzada. Falta terminar algunas animaciones y botones

--- Project/src/app/lista/lista.component.ts
+++ Project/src/app/lista/lista.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-lista',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista.component.css']
 })
 export class ListaComponent {
+  @ViewChild('sidebar') sidebar!: ElementRef;
+  @ViewChild('vineta') vineta! :ElementRef;
 
+  constructor() { }
+
+  mostrarSidebar(): void {
+    this.sidebar.nativeElement.classList.add('show');
+    this.vineta.nativeElement.style.display = 'none';
+  }
+
+  mostrarVineta(): void {
+    if (!this.sidebar.nativeElement.classList.contains('show')) {
+      this.vineta.nativeElement.style.display = 'block';
+    }
+  }
+
+  ocultarSidebar(): void {
+    this.sidebar.nativeElement.classList.remove('show');
+  }
 }
