4cbc20a7c6e587b1b3cc504c30d35b4eaec080ce
Camilodez <hernandezz_camilo@javeriana.edu.co>
2024-04-18 20:50:49 -0500

    coomit

---
+++ KittyClinic/src/app/models/gato.model.ts
@@ -0,0 +1,15 @@
+import { Usuario } from './usuario.model';
+import { Tratamiento } from './tratamiento.model';
+
+export interface  Gato {
+
+    id: number;
+    nombre: string;
+    raza: string;
+    edad: number;
+    foto: string;
+    enfermedad: string;
+    estado: boolean;
+    usuario: number;  
+    tratamientos?: Tratamiento[];  
+}
---
+++ KittyClinic/src/app/services/gato.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Gato} from 'src/app/models/gato.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class GatoService {
+
+  constructor() { }
+
+  
+}
