d85a552d24ffc31612c80bd4cc8f0d0e5d453b21
Michael Gonzalez <michaeljoelgd@gmail.com>
2024-04-22 14:49:14 -0500

    Modificar veterinario funciona

--- src/app/Admin/modificar-veterinario/modificar-veterinario.component.ts
+++ src/app/Admin/modificar-veterinario/modificar-veterinario.component.ts
@@ -10,63 +10,40 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ModificarVeterinarioComponent implements OnInit {
 
-
- 
-
-  veterinario: Veterinario = {
-    id: 0, // Este campo podría no ser necesario en el formulario, dependiendo de tu lógica
-    cedula: 0,
-    nombre: '',
-    apellido: '',
-    password: '',
-    foto: '',
-    especialidad: '',
-  };
-
-  constructor(
-    private veterinarioService: VeterinarioService,
-    private route: ActivatedRoute, 
-    public router: Router
-  ) { }
-
   
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      const id = parseInt(params['id']);
-      if (id === 0) {
-        // Preparar para crear un nuevo usuario
-        console.log('Preparar para crear un nuevo Veterinario');
-      } else {
-        // Cargar información del usuario existente
-        this.displayinfo(id);
-      }
-    });
-  }
+  constructor(private veterinarioService: VeterinarioService,private route: ActivatedRoute, 
+    public router: Router) { 
+      this.route.params.subscribe(params => {
+        this.displayinfo(parseInt(params['id']));
+        console.log(this.veterinario);
+      })
+    }
+    
+    ngOnInit() {
+      
+    }
 
+    veterinario: Veterinario = {
+      id: 0, // Este campo podría no ser necesario en el formulario, dependiendo de tu lógica
+      cedula: 0,
+      nombre: '',
+      apellido: '',
+      password: '',
+      foto: '',
+      especialidad: '',
+    };
 
   
   async displayinfo(id: number) {
-    try {
-      const response = await this.veterinarioService.searchbyId(id);
-      this.veterinario = response.data;
-      console.log(this.veterinario);
-    } catch (error) {
-      console.error('Error cargando el usuario', error);
-    }
+    this.veterinario = (await this.veterinarioService.searchbyId(id)).data;
+    console.log(this.veterinario);
   }
 
 
-  onSubmit(formValues: any) {
-    this.veterinario = Object.assign({}, this.veterinario, formValues);
+  onSubmit(form: any) {
+    this.veterinario = Object.assign({}, this.veterinario);
     console.log(this.veterinario);
     this.veterinarioService.save(this.veterinario);
     this.router.navigate(['/veterinarios']);
   }
-
-
-
-
-
-  
-
 }
--- src/app/services/veterinario.service.ts
+++ src/app/services/veterinario.service.ts
@@ -15,7 +15,7 @@ export class VeterinarioService {
     constructor(private http: HttpClient) { }
 
     findAll(): Promise<AxiosResponse<Veterinario[]>> {
-        return axios.get('http://localhost:8090/Admin/veterinario');
+        return axios.get('http://localhost:8090/admin/veterinario');
     }
 
     add(veterinario: Veterinario): Observable<Veterinario> {
@@ -27,16 +27,14 @@ export class VeterinarioService {
       }
 
 
-      searchbyId(id: number):Promise<AxiosResponse<Veterinario>> {
-        return axios.get<Veterinario>(`http://localhost:8090/admin/veterinario/${id}`);
-      }
+    searchbyId(id: number):Promise<AxiosResponse<Veterinario>> {
+      return axios.get(`http://localhost:8090/admin/veterinario/${id}`);
+    }
     
 
     update(veterinario:Veterinario): Observable<Veterinario> {
         return this.http.put<Veterinario>('http://localhost:8090/admin/update/veterinario', veterinario);
     }
-
-   
 
     delete(id: number): Observable<Veterinario> {
         return this.http.delete<Veterinario>(`http://localhost:8090/admin/delete/${id}`);
